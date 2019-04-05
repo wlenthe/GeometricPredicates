a5ac8f8cd31c962fc23d469b26260c5daf4635ea
Artem Amirkhanov <artem.amirkhanov@leica-geosystems.com>
2019-04-05 12:03:02 +0200

    C++03 compatibility fixes

--- predicates.h
+++ predicates.h
@@ -30,8 +30,8 @@
  *                                                                                 *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-#ifndef _PREDICATES_H_
-#define _PREDICATES_H_
+#ifndef PREDICATES_H_dff5c08
+#define PREDICATES_H_dff5c08
 
 //@reference: https://www.cs.cmu.edu/~quake/robust.html
 
@@ -117,25 +117,6 @@ namespace  predicates {
 	}
 }
 
-// a macro based static assert for pre c++11
-#define MACRO_STATIC_ASSERT(condition, message) typedef char message[(condition) ? 1 : -1];
-	
-// check if we have c++11 features
-#if(__cplusplus == 199711L)
-	//compiler is c++98 or c++03
-#elif( __cplusplus == 201103L || __cplusplus == 201402L || __cplusplus == 201703L || __cplusplus == 201703L || __cplusplus > 201703L)
-	//if the compiler is c++11 (or newer) use some extra functionality
-	#define CXX_ARRAY    //std::array
-	#define CXX_ENABLE_IF//std::enable_if
-	#define CXX_ASSERT   //static_assert
-	#define CXX_COPY_N   //std::copy_n
-	#include <array>
-	#include <type_traits>//is_same, enable_if
-#else
-	//we failed to determine the compiler
-	MACRO_STATIC_ASSERT(false, couldnt_parse_cxx_standard)
-#endif
-
 #include <cmath>//abs, fma
 #include <limits>
 #include <utility>//pair
@@ -143,37 +124,66 @@ namespace  predicates {
 #include <algorithm>//transform, copy_n, merge
 #include <functional>//negate
 
-#ifdef CXX_ARRAY
-	namespace detail {
-		template<class T, size_t N> using Array = std::array<T, N>;//detail::Array<T,N> == std::array<T,N>
-	}
-#else
-	//we need to define a minimal version of std::array
-	namespace detail {
-		//a class to emulate required behavoir of std::array
-		template<typename T, size_t N>
-		class Array {
-			T buff[N];
+// a macro based static assert for pre c++11
+#define PREDICATES_PORTABLE_STATIC_ASSERT(condition, message) typedef char message[(condition) ? 1 : -1]
 
-			public:
-				      T& operator[](const size_t& i)       {return buff[i];} 
-				const T& operator[](const size_t& i) const {return buff[i];} 
-
-				T       * data()       {return buff;}
-				T const * data() const {return buff;}
-
-				T       *  begin()       {return buff;}
-				T const * cbegin() const {return buff;}
-		};
-	}
+// check if c++11 is supported
+#if !defined(__cplusplus) && !defined(_MSC_VER)
+	PORTABLE_STATIC_ASSERT(false, couldnt_parse_cxx_standard)
 #endif
+#if __cplusplus >= 201103L || (defined(_MSC_VER) && _MSC_VER >= 1900)
+	#define PREDICATES_CXX11_IS_SUPPORTED
+#endif
+
+// choose to use c++11 features or their backports
+#ifdef PREDICATES_CXX11_IS_SUPPORTED
+#include <array>
+#include <type_traits>// is_same, enable_if
+#undef PREDICATES_PORTABLE_STATIC_ASSERT
+#define PREDICATES_TOKEN_TO_STRING1(x)  #x
+#define PREDICATES_TOKEN_TO_STRING(x)  PREDICATES_TOKEN_TO_STRING1(x)
+#define PREDICATES_PORTABLE_STATIC_ASSERT(condition, message) static_assert(condition, PREDICATES_TOKEN_TO_STRING(message))
+namespace stdx {
+	using std::array;
+	using std::copy_n;
+}
+#else
+namespace stdx {
+	// array
+	template<typename T, size_t N>
+	class array {
+		T buff[N];
+	public:
+		T& operator[](const size_t& i) { return buff[i]; }
+		const T& operator[](const size_t& i) const { return buff[i]; }
+
+		T       * data() { return buff; }
+		T const * data() const { return buff; }
+
+		T       *  begin() { return buff; }
+		T const * cbegin() const { return buff; }
+	};
+	// copy_n
+	template< class InputIt, class Size, class OutputIt>
+	OutputIt copy_n(InputIt first, Size count, OutputIt result)
+	{
+		if (count > 0) {
+			*result++ = *first;
+			for (Size i = 1; i < count; ++i) {
+				*result++ = *++first;
+			}
+		}
+		return result;
+	}
+}
+#endif // CXX11_IS_SUPPORTED
 
 namespace detail {
 	template<typename T> class ExpansionBase;
 
 	//@brief: class to exactly represent the result of a sequence of arithmetic operations as an sequence of values that sum to the result
 	template<typename T, size_t N>
-	class Expansion : private ExpansionBase<T>, public Array<T, N> {
+	class Expansion : private ExpansionBase<T>, public stdx::array<T, N> {
 		private:
 		public:
 			size_t m_size;
@@ -182,30 +192,23 @@ namespace detail {
 
 			Expansion() : m_size(0) {}
 			template <size_t M> Expansion& operator=(const Expansion<T, M>& e) {
-			#ifdef CXX_ASSERT
-				static_assert(M <= N, "cannot assign a larger expansion to a smaller expansion");
-			#else
-				MACRO_STATIC_ASSERT(M <= N, cannot_assign_a_larger_expansion_to_a_smaller_expansion)
-			#endif
-
-			#ifdef CXX_COPY_N
-				std::copy_n(e.cbegin(), e.size(), Array<T, N>::begin());
-			#else
-				for(size_t i = 0; i < N; i++) this->operator[](i) = e[i];
-			#endif
+				PREDICATES_PORTABLE_STATIC_ASSERT(M <= N, cannot_assign_a_larger_expansion_to_a_smaller_expansion);
+				stdx::copy_n(e.cbegin(), e.size(), stdx::array<T, N>::begin());
 				m_size = e.size();
 				return *this;
 			}
 
+			Expansion(const Expansion& other) { *this = other; }
+
 			//vector like convenience functions
 			size_t size() const {return m_size;}
 			bool empty() const {return 0 == m_size;}
-			void push_back(const T v) {Array<T, N>::operator[](m_size++) = v;}
+			void push_back(const T v) {stdx::array<T, N>::operator[](m_size++) = v;}
 
 		public:
 			//estimates of expansion value
-			T mostSignificant() const {return empty() ? T(0) : Array<T, N>::operator[](m_size - 1);}
-			T estimate() const {return std::accumulate(Array<T, N>::cbegin(), Array<T, N>::cbegin() + size(), T(0));}
+			T mostSignificant() const {return empty() ? T(0) : stdx::array<T, N>::operator[](m_size - 1);}
+			T estimate() const {return std::accumulate(stdx::array<T, N>::cbegin(), stdx::array<T, N>::cbegin() + size(), T(0));}
 
 			template <size_t M> Expansion<T, N+M> operator+(const Expansion<T, M>& f) const {
 				Expansion<T, N+M> h;
@@ -213,7 +216,7 @@ namespace detail {
 				return h;
 			}
 
-			void negate() {std::transform(Array<T, N>::cbegin(), Array<T, N>::cbegin() + size(), Array<T, N>::begin(), std::negate<T>());}
+			void negate() {std::transform(stdx::array<T, N>::cbegin(), stdx::array<T, N>::cbegin() + size(), stdx::array<T, N>::begin(), std::negate<T>());}
 			Expansion operator-() const {Expansion e = *this; e.negate(); return e;}
 			template <size_t M> Expansion<T, N+M> operator-(const Expansion<T, M>& f) const {return operator+(-f);}
 
@@ -243,7 +246,7 @@ namespace detail {
 		static const bool fp_fast_fmal = false;
 	#endif
 
-	#ifdef CXX_ENABLE_IF
+	#ifdef PREDICATES_CXX11_IS_SUPPORTED
 	template <typename T> struct use_fma {static const bool value = (std::is_same<T, float>::value       && fp_fast_fmaf) ||
 	                                                                (std::is_same<T, double>::value      && fp_fast_fma)  ||
 	                                                                (std::is_same<T, long double>::value && fp_fast_fmal);};
@@ -259,15 +262,12 @@ namespace detail {
 	template<typename T>
 	class ExpansionBase {
 		private:
-			static const T Splitter;
+			typedef unsigned long long IType;//integer type for splitter calculation
+			static const IType Splitter = ( IType(1) << ( (std::numeric_limits<T>::digits + 1 ) / 2 ) ) + 1;
 
-		#ifdef CXX_ASSERT
-			static_assert(std::numeric_limits<T>::is_iec559, "Requires IEC 559 / IEEE 754 floating point type");
-			static_assert(2 == std::numeric_limits<T>::radix, "Requires base 2 floating point type");
-		#else
-			MACRO_STATIC_ASSERT(std::numeric_limits<T>::is_iec559, Requires_IEC_559_IEEE_754_floating_point_type);
-			MACRO_STATIC_ASSERT(2 == std::numeric_limits<T>::radix, Requires_base_2_floating_point_type);
-		#endif
+			PREDICATES_PORTABLE_STATIC_ASSERT(std::numeric_limits<T>::is_iec559, Requires_IEC_559_IEEE_754_floating_point_type);
+			PREDICATES_PORTABLE_STATIC_ASSERT(2 == std::numeric_limits<T>::radix, Requires_base_2_floating_point_type);
+			PREDICATES_PORTABLE_STATIC_ASSERT(sizeof(IType) >= sizeof(T), Integer_type_may_overflow_during_splitter_calculation);
 
 			//combine result + roundoff error into expansion
 			static inline Expansion<T, 2> MakeExpansion(const T value, const T tail) {
@@ -355,7 +355,7 @@ namespace detail {
 			}
 
 			//roundoff error of x = a * b via dekkers product
-			static inline T DekkersProduct(const T a, const std::pair<T, T> aSplit, const T b, const std::pair<T, T> bSplit, const T p) {
+			static inline T DekkersProduct(const T /*a*/, const std::pair<T, T> aSplit, const T /*b*/, const std::pair<T, T> bSplit, const T p) {
 				T y = p - T(aSplit.first * bSplit.first);
 				y -= T(aSplit.second * bSplit.first);
 				y -= T(aSplit.first * bSplit.second);
@@ -363,18 +363,16 @@ namespace detail {
 			}
 
 			//roundoff error of x = a * b
-		#ifdef CXX_ENABLE_IF
+#ifdef PREDICATES_CXX11_IS_SUPPORTED
 			template <typename S = T> static typename std::enable_if< use_fma<S>::value, S>::type MultTail(const T a, const T b, const T p) {return std::fma(a, b, -p);}
 			template <typename S = T> static typename std::enable_if<!use_fma<S>::value, S>::type MultTail(const T a, const T b, const T p) {return DekkersProduct(a, Split(a), b, Split(b), p);}
 
-			template <typename S = T> static typename std::enable_if< use_fma<S>::value, S>::type MultTailPreSplit(const T a, const T b, const std::pair<T, T> bSplit, const T p) {return std::fma(a, b, -p);}
+			template <typename S = T> static typename std::enable_if< use_fma<S>::value, S>::type MultTailPreSplit(const T a, const T b, const std::pair<T, T> /*bSplit*/, const T p) {return std::fma(a, b, -p);}
 			template <typename S = T> static typename std::enable_if<!use_fma<S>::value, S>::type MultTailPreSplit(const T a, const T b, const std::pair<T, T> bSplit, const T p) {return DekkersProduct(a, Split(a), b, bSplit, p);}
-		#else
-			//std::fma shouldn't exist unless std::enable_if is also defined
+#else
 			static T MultTail(const T a, const T b, const T p) {return DekkersProduct(a, Split(a), b, Split(b), p);}
 			static T MultTailPreSplit(const T a, const T b, const std::pair<T, T> bSplit, const T p) {return DekkersProduct(a, Split(a), b, bSplit, p);}
-		#endif
-
+#endif
 			//expand a + b
 			static inline Expansion<T, 2> Plus(const T a, const T b) {
 				const T x = a + b;
@@ -425,8 +423,6 @@ namespace detail {
 			//(a * b) * c checking for zeros
 			static inline Expansion<T, 4> ThreeProd(const T a, const T b, const T c) {return (T(0) == a || T(0) == b || T(0) == c) ? Expansion<T, 4>() : Mult(a, b) * c;}
 	};
-
-	template <typename T> const T ExpansionBase<T>::Splitter = std::ldexp(T(1), (std::numeric_limits<double>::digits + 1 ) / 2 ) + T(1);
 }
 
 namespace  predicates {
@@ -560,8 +556,11 @@ namespace  predicates {
 			static const T iccerrboundA, iccerrboundB, iccerrboundC;
 			static const T isperrboundA, isperrboundB, isperrboundC;
 	};
-
-	template <typename T> const T Constants<T>::epsilon        = std::ldexp(T(1), -std::numeric_limits<T>::digits);
+#ifdef PREDICATES_CXX11_IS_SUPPORTED
+	template <typename T> const T Constants<T>::epsilon        = std::exp2(T(-std::numeric_limits<T>::digits));
+#else
+	template <typename T> const T Constants<T>::epsilon        = std::pow(2, T(-std::numeric_limits<T>::digits));
+#endif
 	template <typename T> const T Constants<T>::resulterrbound = (T( 3) + T(   8) * Constants<T>::epsilon) * Constants<T>::epsilon;
 	template <typename T> const T Constants<T>::ccwerrboundA   = (T( 3) + T(  16) * Constants<T>::epsilon) * Constants<T>::epsilon;
 	template <typename T> const T Constants<T>::ccwerrboundB   = (T( 2) + T(  12) * Constants<T>::epsilon) * Constants<T>::epsilon;
